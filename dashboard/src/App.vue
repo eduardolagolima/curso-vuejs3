6e268794b541321fc2e9ea970c2dca3df2dee044
eduardolagolima <eduardolagolima@gmail.com>
2022-08-30 20:15:56 -0300

    create login and create account modals

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -4,10 +4,28 @@
 </template>
 
 <script>
-
+import { watch } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
 import ModalFactory from './components/ModalFactory/index.vue';
+import services from './services';
+import { setCurrentUser } from './store/user';
 
 export default {
   components: { ModalFactory },
+  setup() {
+    const router = useRouter();
+    const route = useRoute();
+    watch(() => route.path, async () => {
+      if (route.meta.hasAuth) {
+        const token = window.localStorage.getItem('token');
+        if (!token) {
+          router.push({ name: 'Home' });
+          return;
+        }
+        const { data } = await services.users.getMe();
+        setCurrentUser(data);
+      }
+    });
+  },
 };
 </script>
